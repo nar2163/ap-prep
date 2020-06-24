50082469e47c93da4ab28345bb6b2a232d7b43c4
Nicolas Rockwood <nar2163@columbia.edu>
2020-06-23 20:56:13 -0400

    practice for june 23

--- letterCountHist.c
+++ letterCountHist.c
@@ -3,4 +3,31 @@
 int main(){
 
 	int c;
+	int charCounts[26] = {0};
+	int count = 0;
+
+
+	while((c = getchar()) != '\n'){
+		charCounts[c - 'a']++;
+	}
+
+	char letter = 'a'; 
+        
+	for(int i = 0; i < 26; i++){
+		if(charCounts[i] == 0){
+			charCounts[i] = 0;
+		}
+	}
+
+	for(int i = 0; i < 26; i++){
+		printf("%c: ", letter);
+		for(int j = 0; j < charCounts[i]; j++){
+			printf("X");
+		}
+		printf("\n");
+		letter++;
+	}
+
+	return 0;
+}
 	
